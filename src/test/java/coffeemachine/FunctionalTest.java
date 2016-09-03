4bfe9b2e397421967634b025f44eb600131af5ed
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-09-03 22:58:24 +0200

    rename to FunctionalTest

--- src/test/java/coffeemachine/FunctionalTest.java
+++ src/test/java/coffeemachine/FunctionalTest.java
@@ -6,7 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class LogicTest {
+public class FunctionalTest {
 
     private Mockery context;
     private DrinkMaker drinkMaker;
